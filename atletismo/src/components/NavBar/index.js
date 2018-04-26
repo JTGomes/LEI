4b0263ea8629f96c5767e2efdf78ea36c1de0f43
JTGomes <tiago.gomez@hotmail.com>
2018-04-26 10:02:58 +0100

    correçao sideNav

--- atletismo/src/components/NavBar/index.js
+++ atletismo/src/components/NavBar/index.js
@@ -12,7 +12,7 @@ class NavBar extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
     };
 
     this.toggle = this.toggle.bind(this);
@@ -32,7 +32,7 @@ class NavBar extends Component{
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <div className={"collapse navbar-collapse justify-content-end"} id="navbarSupportedContent">
           <ul className="nav navbar-nav navbar-right">
             <li className="nav-item" style={{margin:"0 5px"}}>
               <Notificacao />
